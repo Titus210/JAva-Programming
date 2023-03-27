cbc061a10359e4b8b484ff6e8c4a001e195f74f0
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-03-27 09:03:00 +0300

    Arithmetic Operators

---
+++ Lessons/Drive/Operators/ArithmeticOperations.java
@@ -0,0 +1,25 @@
+package Lessons.Drive.Operators;
+
+public class ArithmeticOperations {
+    
+        public static void main(String[] args) {
+            // declare variables
+            int a = 10;
+            int b = 20;
+
+            // addition
+            System.out.println("a + b = " + (a + b));
+
+            // subtraction
+            System.out.println("a - b = " + (a - b));
+
+            // multiplication
+            System.out.println("a * b = " + (a * b));
+
+            // Division
+            System.out.println("a / b = " + (a / b));
+
+            // modulo
+            System.out.println("a % b = " + (a % b));
+        }
+    }
