b26d819a1271a4d43156f0b9e4c3e1601e775d0c
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-03-30 19:35:26 +0300

    Age

--- 100+_Programs/ageCalculator/age.java
+++ 100+_Programs/ageCalculator/age.java
@@ -1,11 +1,16 @@
 import java.util.Scanner;
+import java.util.Calendar;
+import java.sql.Date;
 
 public class age {
     static Scanner reader = new Scanner(System.in);
-    public static void main(String[] args){
-    System.out.print("Enter your Date of birth: ");;
+
+    public static void main(String[] args) {
+        System.out.print("Enter your Date of birth: ");
+
         int DOB = reader.nextInt();
-        int age = 2023 - DOB;
+        int year = Calendar.getInstance().get(Calendar.YEAR);
+        int age = year - DOB;
         System.out.println("Your age is: " + age + "  years");
     }
 }
