88e52585045c391c514c9968ed519e652312b280
Titus Kiplagat <tituskiplagat210@gmail.com>
2023-04-15 10:04:23 +0300

    Starred Square Modified

--- 100+_Programs/Starred Square/StarSquare.java
+++ 100+_Programs/Starred Square/StarSquare.java
@@ -17,5 +17,15 @@ class StarSquare {
                 System.out.println("*********");
             }
         }
+
+        System.out.println("second Square");
+        for ( i = 1; i <= 5; i++) // outer loop controlling the number of rows
+        {
+            for (int j = 1; j <= i; j++) // inner loop controlling the number of stars in one row
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
