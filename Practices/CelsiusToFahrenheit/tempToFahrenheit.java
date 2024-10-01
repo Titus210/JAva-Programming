cef6f5d4b2608d3cd4e3d532c4b8879b1171aeef
Titus210 <tituskiplagat210@gmail.com>
2024-10-01 23:45:25 +0300

    feat: getting user input and changing temo to farenheight

--- Practices/CelsiusToFahrenheit/tempToFahrenheit.java
+++ Practices/CelsiusToFahrenheit/tempToFahrenheit.java
@@ -1,7 +1,12 @@
 // Write a Java program that converts a temperature from Celsius to Fahrenheit and vice versa.// //////
+import java.util.Scanner;
+
 public class tempToFahrenheit{
     public static void main(String[] args){
-        double celsius = 37.5;
+        // double celsius = 37.5; replace this with getting user input from keyboard
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the temperature in Celsius:");
+        double celsius = scanner.nextDouble();
         double fahrenheit = (celsius * 9/5) + 32;
         System.out.println(celsius + "°C is " + fahrenheit + "°F");
     }
