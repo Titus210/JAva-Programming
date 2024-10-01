cdb20dfd189f33d299a25e42ac82fc6b2c6d19d3
Titus210 <tituskiplagat210@gmail.com>
2024-10-01 23:36:47 +0300

    feat: print Variables class and Java
    This initializes variables and prints them in console

---
+++ Practices/PrintVariables/printVariables.java
@@ -0,0 +1,18 @@
+// Declare variables of different data types (int, double, char, String, boolean) and print their values.
+package Practices.PrintVariables;
+
+public class printVariables{
+public static void main(String[] args) {
+    int a = 10;
+    double b = 10.5;
+    char c = 'A';
+    String d = "Hello";
+    boolean e = true;
+
+    System.out.println("Integer: " + a);
+    System.out.println("Double: " + b);
+    System.out.println("Character: " + c);
+    System.out.println("String: " + d);
+    System.out.println("Boolean: " + e);
+}
+}
